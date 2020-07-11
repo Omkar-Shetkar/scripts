6e6f68a5ceabc3e0316ce44147d9a3934b1069b9
Omkar Shetkar <omkar.shetkar@outlook.com>
2020-07-11 23:05:13 +0530

    unzip spring boot project and open in intellij idea

---
+++ bootidea.sh
@@ -0,0 +1,9 @@
+# Unzips Spring Boot .zip project and opens the folder in IntelliJ Idea
+
+unzip $1
+rm $1
+NAME=$(echo $1 | cut -d'.' -f 1)
+echo $NAME
+intellij-idea-community $NAME 2> /dev/null &
+
+

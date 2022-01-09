b23155b5a5323b7c2258423e35d347bac00f6c0b
Omkar Shetkar <omkar.shetkar@outlook.com>
2022-01-09 06:13:37 +0530

    obsidian scripts added

---
+++ launch-obsidian.bash
@@ -0,0 +1,6 @@
+#! /bin/bash
+cd /Users/omkar/learn/learn-doc
+git pull
+/Applications/Obsidian.app/Contents/MacOS/Obsidian & > /dev/null
+
+
---
+++ update-obsidian.bash
@@ -0,0 +1,9 @@
+#! /bin/bash
+cd /Users/omkar/learn/learn-doc
+git pull
+git add .
+echo "Enter comment for the commit:"
+read comment
+git commit -m $comment
+git push
+

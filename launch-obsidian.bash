86f363561e65befb699ae8963c45001c7bb3424d
Omkar Shetkar <omkar.shetkar@outlook.com>
2022-01-23 13:10:52 +0530

    git stash before opening obsidian

--- launch-obsidian.bash
+++ launch-obsidian.bash
@@ -1,5 +1,6 @@
 #! /bin/bash
 cd /Users/omkar/learn/learn-doc
+git stash
 git pull
 /Applications/Obsidian.app/Contents/MacOS/Obsidian & > /dev/null
 
--- update-obsidian.bash
+++ update-obsidian.bash
@@ -4,6 +4,6 @@ git pull
 git add .
 echo "Enter comment for the commit:"
 read comment
-git commit -m $comment
+git commit -m "$comment"
 git push
 
